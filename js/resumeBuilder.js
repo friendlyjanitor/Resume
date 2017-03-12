586d0cecd3cfe805313b52253fe35fe9c24c9701
Ryan Cross <cross.w.ryan@gmail.com>
2017-03-12 12:13:06 -0400

    adding more info to the files

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,4 @@
-// $("#main").append("Ryan Cross");
+$("#main").append("Ryan Cross");
 
 // var awesomeThoughts = "I am Ryan and I am Awesome";
  //var funThoughts = awesomeThoughts.replace ("Awesome", "fun");
@@ -7,37 +7,25 @@
 
  //$("#main").append(funThoughts)
 
-var fullName = "Ryan Cross";
-var job = "SQA Analyst";
+//var fullName = "Ryan Cross";
+//var job = "SQA Analyst";
 
- var name = HTMLheaderName.replace("%data%",fullName);
+ //var name = HTMLheaderName.replace("%data%",fullName);
 
- $("#header").append(name);
+ //$("#header").append(name);
 
 
-var careerTitle = HTMLheaderRole.replace("%data%",job);
-$("#header").append(careerTitle);
+//var careerTitle = HTMLheaderRole.replace("%data%",job);
+//$("#header").append(careerTitle);
 
 
 var skills = ["Jira", "SQL","Linux","JavaScript","Git"];
 
-var bio = {
-	"name" = "Ryan",
-	"role" = "QA Analyst",
-	"contact_info" = "802491690",
-	"welcome message" = "welcome to my resume"
-}
+//var bio = {
+//	"name" = "Ryan",
+//	"role" = "QA Analyst",
+//	"contact_info" = "802491690",
+//	"welcome message" = "welcome to my resume"
+//}
 
-var work = {
-	"employer"="Market Leader",
-	"years" = "3",
-	"Location"= "Bellevue, WA",
-	"Position"="SQA Analyst"
-}
-var education = {
-	"School" = "Plymouth State University",
-	"Degree"="Mathematics",
-	"School_City" = "Plymouth, NH"
-}
-$("#header").append(bio.contact_info);
 console.log(skills.length);
