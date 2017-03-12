50448038a6c81c77262e788c37df17168603d567
Ryan Cross <cross.w.ryan@gmail.com>
2017-03-12 15:52:52 -0400

    working with json

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -21,11 +21,75 @@ $("#header").append(formattedjob);
 
 var skills = ["Jira", "SQL","Linux","JavaScript","Git"];
 
-//var bio = {
-//	"name" = "Ryan",
-//	"role" = "QA Analyst",
-//	"contact_info" = "802491690",
-//	"welcome message" = "welcome to my resume"
-//}
 
-console.log(skills.length);
+
+var WorkExpierence = {
+	"Expierence":[
+		{
+			"Company": "Bullhorn",
+			"Duration": "January 2013 - December 2013",
+			"Location": "Boston, MA",
+			"Title": "Technical Support Analyst"
+		},
+		{
+			"Company": "Market Leader",
+			"Duration": "December 2013 - December 2016",
+			"Location": "Bellevue, WA",
+			"Title": "Software Quality Assurnace Analyst"
+
+		}
+
+
+	]
+}
+
+var Education = {
+	"School": [{
+		"Name": "Plymouth State University",
+		"Location": "Plymouth, NH",
+		"Degree": "B.S. Mathematics",
+		"Year": "2012"
+	}],
+
+	"Courses": [{
+
+		"Name": "JavaScript Basic",
+		"School": "Udacity",
+		"Duration": "Spring 2017"
+	}, {
+		"Name": "Introduction to Programming",
+		"School": "University of Phoneix",
+		"Duration": "winter 2013"
+	}, {
+		"Name": "Intro to C++",
+		"School": "University of Phoneix",
+		"Duration": "winter 2013"
+	}]
+
+}
+
+var bio = {
+	"GivenName" : "Ryan Cross",
+	"role" : "Software Quality Assurnace Analyst",
+	"age" : "27",
+	"skills" :["Jira", "SQL","Linux","JavaScript","Git"]
+};
+	
+
+
+//var s = "audacity";
+
+//var udacityizer = function(s) {  
+    // Right now, the variable s === "audacity"
+    // Manipulate s to make it equal to "Udacity"
+    // Your code goes here!
+    
+  //  s = s.slice(1,2).toUpperCase()+s.slice(2,8);
+    
+   
+    
+    //return s;
+//};
+
+// Did your code work? The line below will tell you!
+// console.log(udacityizer(s));
