384cd0262ac31d473727289dee0fa9ca1d741e42
Ryan Cross <cross.w.ryan@gmail.com>
2017-03-12 11:15:36 -0400

    adding my js file

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,31 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// $("#main").append("Ryan Cross");
+
+// var awesomeThoughts = "I am Ryan and I am Awesome";
+ //var funThoughts = awesomeThoughts.replace ("Awesome", "fun");
+ //console.log (awesomeThoughts);
+ //console.log (funThoughts);
+
+ //$("#main").append(funThoughts)
+
+var fullName = "Ryan Cross";
+var job = "SQA Analyst";
+
+ var name = HTMLheaderName.replace("%data%",fullName);
+
+ $("#header").append(name);
+
+
+var careerTitle = HTMLheaderRole.replace("%data%",job);
+$("#header").append(careerTitle);
+
+
+var skills = ["Jira", "SQL","Linux","JavaScript","Git"];
+
+var bio = {
+	"name" = "Ryan",
+	"role" = "QA Analyst",
+	"contact_info" = "802491690",
+	"welcome message" = "welcome to my resume"
+}
+$("#header").append(bio.contact_info);
+console.log(skills.length);
